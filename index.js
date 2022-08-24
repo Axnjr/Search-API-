9e20244ccfe0a8abdd00034d6428c86a7c96c7dd
Yakshit Chhipa <111436589+Axnjr@users.noreply.github.com>
2022-08-24 21:35:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.js';
+import GoogleAssistant from './GoogleAssistant.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  //<React.StrictMode> // using strict mode renders components twice for finding bugs
-    <App/>    // only in devolpmental process 
-  //</React.StrictMode>
-);
-
-/*const roottwo = ReactDOM.createRoot(document.getElementById('roottwo'));
-roottwo.render(
-  <React.StrictMode>
-    <Searching />
+  <React.StrictMode> 
+    <GoogleAssistant/>    
   </React.StrictMode>
-);*/
-
-
-
+);
